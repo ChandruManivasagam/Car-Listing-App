5c4930836382a973c3e2c496f3aef7a8d08a8945
ChandruManivasagam <123437563+ChandruManivasagam@users.noreply.github.com>
2023-01-24 10:43:21 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CarListApp.Maui.ViewModels;
+namespace CarListApp.Maui;
+
+public partial class MainPage : ContentPage
+{
+    //private readonly CarListViewModel carListViewModel;
+    //int count = 0;
+
+	public MainPage(CarViewModel carListViewModel)
+	{
+		InitializeComponent();
+        BindingContext = carListViewModel;
+
+        Preferences.Set("saveDetails", true);
+        var detailsSaved = Preferences.Get("saveDetails", false);
+    }
+
+    
+}
+
---
+++ MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using CarListApp.Maui.Services;
+using CarListApp.Maui.ViewModels;
+using CarListApp.Maui.Views;
+
+namespace CarListApp.Maui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		string dbpath = Path.Combine(FileSystem.AppDataDirectory, "cars.db3");
+		builder.Services.AddSingleton(s => ActivatorUtilities.CreateInstance<CarService>(s, dbpath));
+
+
+
+        builder.Services.AddSingleton<CarService>();
+        builder.Services.AddSingleton<CarViewModel>();
+        builder.Services.AddTransient<CarDetailsViewModel>();//new instance for everytime
+        builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddTransient<CarDetailsPage>();
+        return builder.Build();
+	}
+}
